0f1373cae774a88d9fc33887547ceefc30328215
duanzhiwei <89340545@qq.com>
2020-04-25 15:12:23 +0800

    'bak'

--- src/views/layouts/UserLayout.vue
+++ src/views/layouts/UserLayout.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="wrapper" :style="{  backgroundImage: 'url(/img/bg1.jpg)'}">
+	<div id="wrapper" :style="{  backgroundImage: 'url(img/bg1.jpg)'}">
 		<div class="container">
 			<div title="返回首页" class="logo"></div>
 			<transition name="bounce" mode="out-in">
